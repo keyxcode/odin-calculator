bbb687a0d12461b17a1af6081e658f30ef6aa6b5
keyxcode <keyxcode@gmail.com>
2023-01-30 00:20:43 +0700

    implement multiple-digit number input

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
 
 document.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
-    let screen = document.querySelector("#screen");
+    let calcScreen = document.querySelector("#screen");
     
-    if (button.classList.contains("number") && operator == undefined) {
-        num1 = parseInt(button.textContent);
-        screen.textContent = num1;
+    if (button.classList.contains("number") && operator == "") {
+        num1 += button.textContent;
+        calcScreen.textContent = num1;
     }
     else if(button.classList.contains("number")) {
-        num2 = parseInt(button.textContent);
-        screen.textContent = num2;
+        num2 += button.textContent;
+        calcScreen.textContent = num2;
     }
     else if (button.classList.contains("operator")) {
         operator = button.textContent;
-        screen.textContent = operator;
+        calcScreen.textContent = operator;
     }
     else if (button.id === "equal-operator") {
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
         result = calculate(num1, num2, operator);
         num1 = result;
-        screen.textContent = result;
+        operator = "";
+        calcScreen.textContent = result;
     }
 }))
 
@@ -35,10 +38,6 @@ function calculate(a, b, operator) {
             break;
     }
 
-    console.log(a);
-    console.log(b);
-    console.log(operator);
-    console.log(result);
     return result;
 }
 
