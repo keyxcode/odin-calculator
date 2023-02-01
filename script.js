0e316059a449a6b7a2be52a1bae0015d8eb290cc
keyxcode <keyxcode@gmail.com>
2023-02-01 16:12:36 +0700

    fix logic for chaining operators

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let num1 = 0;
+let num1 = "";
 let num2 = "";
 let operator = "";
 
@@ -7,27 +7,53 @@ document.querySelectorAll("button").forEach(button => button.addEventListener("c
     let screenNumber = document.querySelector("#screen-number");
     
     if (button.classList.contains("number")) {
+        // Only allow 1 decimal symbol
         if (button.id === "decimal" && screenNumber.textContent.includes(".")) return;
         screenNumber.textContent += button.textContent;
     }
     else if (button.classList.contains("operator")) {
-        num1 = parseFloat(screenNumber.textContent)
         operator = button.textContent;
         screenEquation.textContent = `${num1} ${operator}`;
-        screenNumber.textContent = "";
+
+        if (!num1 && !num2) {
+            num1 = parseFloat(screenNumber.textContent);
+
+            screenEquation.textContent = `${num1} ${operator}`;
+            screenNumber.textContent = "";
+        } else if (!num2) {
+            num2 = parseFloat(screenNumber.textContent);
+            if (!num2) return;
+            result = calculate(num1, num2, operator);
+
+            screenEquation.textContent = `${result} ${operator}`;
+            screenNumber.textContent = "";
+            num1 = result;
+            num2 = "";
+        } else {
+            result = calculate(num1, num2, operator);
+            screenEquation.textContent = calculate(num1, num2, operator);
+            screenNumber.textContent = "";
+
+            num1 = result;
+            num2 = "";
+        }
     }
     else if (button.id === "equal-operator") {
         if (operator) num2 = parseFloat(screenNumber.textContent)
         else return;
 
         result = calculate(num1, num2, operator);
-        screenNumber.textContent = result;
         screenEquation.textContent = `${num1} ${operator} ${num2} =`
+        screenNumber.textContent = result;
     
         num1 = result;
         operator = "";
     }
 }))
+
+document.querySelector("#ac").addEventListener('click', () => {
+    clear();
+})
 
 function calculate(a, b, operator) {
     if (!a && !b && !operator) return 0
@@ -50,4 +76,13 @@ function calculate(a, b, operator) {
     }
 
     return result;
+}
+
+function clearScreen() {
+    document.querySelector("#screen-equation").textContent = "";
+    document.querySelector("#screen-number").textContent = "";
+
+    num1 = "";
+    num2 = "";
+    operator = "";
 }
