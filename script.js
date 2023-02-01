2a7d2397a5cc852fa7bf75163a4aef276d034dd6
keyxcode <keyxcode@gmail.com>
2023-02-01 16:17:59 +0700

    separate buttons query selector to make code more readable

--- script.js
+++ script.js
@@ -2,54 +2,54 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 
-document.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
-    let screenEquation = document.querySelector("#screen-equation");
-    let screenNumber = document.querySelector("#screen-number");
-    
-    if (button.classList.contains("number")) {
-        // Only allow 1 decimal symbol
-        if (button.id === "decimal" && screenNumber.textContent.includes(".")) return;
-        screenNumber.textContent += button.textContent;
-    }
-    else if (button.classList.contains("operator")) {
-        operator = button.textContent;
+const screenEquation = document.querySelector("#screen-equation");
+const screenNumber = document.querySelector("#screen-number");
+
+document.querySelectorAll(".number").forEach(button => button.addEventListener('click', () => {
+    // Only allow 1 decimal symbol
+    if (button.id === "decimal" && screenNumber.textContent.includes(".")) return;
+    screenNumber.textContent += button.textContent;
+}))
+
+document.querySelectorAll(".operator").forEach(button => button.addEventListener("click", () => {
+    operator = button.textContent;
+    screenEquation.textContent = `${num1} ${operator}`;
+
+    if (!num1 && !num2) {
+        num1 = parseFloat(screenNumber.textContent);
+
         screenEquation.textContent = `${num1} ${operator}`;
-
-        if (!num1 && !num2) {
-            num1 = parseFloat(screenNumber.textContent);
-
-            screenEquation.textContent = `${num1} ${operator}`;
-            screenNumber.textContent = "";
-        } else if (!num2) {
-            num2 = parseFloat(screenNumber.textContent);
-            if (!num2) return;
-            result = calculate(num1, num2, operator);
-
-            screenEquation.textContent = `${result} ${operator}`;
-            screenNumber.textContent = "";
-            num1 = result;
-            num2 = "";
-        } else {
-            result = calculate(num1, num2, operator);
-            screenEquation.textContent = calculate(num1, num2, operator);
-            screenNumber.textContent = "";
-
-            num1 = result;
-            num2 = "";
-        }
-    }
-    else if (button.id === "equal-operator") {
-        if (operator) num2 = parseFloat(screenNumber.textContent)
-        else return;
-
+        screenNumber.textContent = "";
+    } else if (!num2) {
+        num2 = parseFloat(screenNumber.textContent);
+        if (!num2) return;
         result = calculate(num1, num2, operator);
-        screenEquation.textContent = `${num1} ${operator} ${num2} =`
-        screenNumber.textContent = result;
-    
+
+        screenEquation.textContent = `${result} ${operator}`;
+        screenNumber.textContent = "";
         num1 = result;
-        operator = "";
+        num2 = "";
+    } else {
+        result = calculate(num1, num2, operator);
+        screenEquation.textContent = calculate(num1, num2, operator);
+        screenNumber.textContent = "";
+
+        num1 = result;
+        num2 = "";
     }
 }))
+
+document.querySelector("#equal-operator").addEventListener('click', () => {
+    if (operator) num2 = parseFloat(screenNumber.textContent)
+    else return;
+
+    result = calculate(num1, num2, operator);
+    screenEquation.textContent = `${num1} ${operator} ${num2} =`
+    screenNumber.textContent = result;
+
+    num1 = result;
+    operator = "";
+})
 
 document.querySelector("#ac").addEventListener('click', () => {
     clear();
@@ -79,10 +79,9 @@ function calculate(a, b, operator) {
 }
 
 function clearScreen() {
-    document.querySelector("#screen-equation").textContent = "";
-    document.querySelector("#screen-number").textContent = "";
-
     num1 = "";
     num2 = "";
     operator = "";
+    screenEquation.textContent = "";
+    screenEquation.textContent = "";
 }
